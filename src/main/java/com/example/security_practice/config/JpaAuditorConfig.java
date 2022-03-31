15805f2230d38749a7f42a73e4cbb6dab44384c3
KBS <qudtjs3636@gmail.com>
2022-03-31 15:46:59 +0900

    Add #1 - Config
    Config 코드 작성

---
+++ src/main/java/com/example/security_practice/config/JpaAuditorConfig.java
@@ -0,0 +1,13 @@
+package com.example.security_practice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @Author: kbs
+ */
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditorConfig {
+}
